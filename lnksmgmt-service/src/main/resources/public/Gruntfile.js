b2f0c61c6e07a0cb42950838cab4529504891503
tjangrot <tomek.jangrot@gmail.com>
2015-08-07 11:49:00 +0200

    - added grunt and bower - added app.js - added bootstrap template

---
+++ lnksmgmt-service/src/main/resources/public/Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js', 'js/**/*.js'
+            ]
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+};
